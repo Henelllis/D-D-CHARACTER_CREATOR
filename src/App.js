fd276fb88ded5c77c6392ebe399c2876b8d9afbc
Henelllis <hellis1001@gmail.com>
2019-01-03 22:15:05 -0500

    Adjusting SideBar and Starting Routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 import './App.css';
 import './components/SideBar';
 import SideBar from './components/SideBar';
-
+import PointsBuyScreen from './containers/PointsBuyScreen';
 class App extends Component {
 
 
@@ -13,19 +13,16 @@ class App extends Component {
 
     let routes = (
       <Switch>
-        <Route path='/'/>
-        <Route path='/Points_Buy_In'/> 
-        <Route path='/Player Race'/>
-        <Route path='/Character Background'/>
-        <Route path='Pick Your Class'/>     
+        <Route path='/' component={PointsBuyScreen}/>
+        {/* <Route path='/Points_Buy_In' component={PointsBuyScreen}/> 
+        <Route path='/Player Race' component={PointsBuyScreen}/>
+        <Route path='/Character Background' component={PointsBuyScreen}/>
+        <Route path='Pick Your Class' component={PointsBuyScreen}/>      */}
       </Switch>
     ) 
 
     return (
       <div className="App">
-        <header className="header_container">
-          header
-        </header>
         <SideBar />
            {routes}
          <footer />
---
+++ src/containers/PointsBuyScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class PointsBuyScreen extends Component{
+    state={
+
+    }
+
+    render(){
+        return(
+            <div className="points_buy_in_screen">
+                BUY SOME POINTS YOU DINGUS
+            </div>
+        )
+    }
+};
+
+export default PointsBuyScreen;
